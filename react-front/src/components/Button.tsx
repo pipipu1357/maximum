76eb32d44b87f747d15a7e1af618fb1259c4a599
pipipu1357 <pipipu.1357@gmail.com>
2024-12-29 19:21:58 +0900

    ホルモン曲検索のトップ画面から 曲をさがす場所に遷移するようにいたしました。

--- react-front/src/components/Button.tsx
+++ react-front/src/components/Button.tsx
@@ -1,22 +1,27 @@
-import React from "react";
+import React from 'react'
 
 interface ButtonProps {
-    label: string;
-    onClick: () => void;
-    type?: "button" | "submit" | "reset";
-    className?: string;
+  label: string
+  onClick: () => void
+  type?: 'button' | 'submit' | 'reset'
+  className?: string
 }
 
-const Button = ({ label, onClick, type = "button", className = '' }: ButtonProps) => {
-    return (
-        <button
-            type={type}
-            onClick={onClick}
-            className={`px-4 py-2 bg-blue-500 text-white rounded-md ${className}`}
-        >
-            {label}
-        </button>
-    );
-};
+const Button = ({
+  label,
+  onClick,
+  type = 'button',
+  className = '',
+}: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`px-4 py-2 bg-blue-500 text-white rounded-md ${className}`}
+    >
+      {label}
+    </button>
+  )
+}
 
-export default Button;
+export default Button
--- react-front/src/pages/App.tsx
+++ react-front/src/pages/App.tsx
@@ -1,10 +1,17 @@
-import React from "react";
-import UserHomePages from "../pages/Home/UserHomePages";
+import React from 'react'
+import UserHomePages from './home/UserHomePages'
+import UserSearchSongPages from './search/UserSearchSongPages'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App: React.FC = () => {
-    return (
-        <UserHomePages title="ユーザーのホームページ" />
-    );
-};
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<UserHomePages title="ホルモン曲検索" />} />
+        <Route path="/search" element={<UserSearchSongPages />} />
+      </Routes>
+    </Router>
+  )
+}
 
-export default App;
+export default App
