347e512a5605f653632db51c16ee2bed3ee483ac
pipipu1357 <pipipu.1357@gmail.com>
2025-01-25 13:12:42 +0900

    ディスコグラフィーの画面を作成いたしました。

--- react-front/src/pages/App.tsx
+++ react-front/src/pages/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import UserHomePages from './home/UserHomePages'
 import UserSearchSongPages from './search/UserSearchSongPages'
+import UserDiscographyPages from './discography/UserDiscographyPages'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App: React.FC = () => {
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<UserHomePages title="ホルモン曲検索" />} />
         <Route path="/search" element={<UserSearchSongPages />} />
+        <Route path="/discography" element={<UserDiscographyPages />} />
       </Routes>
     </Router>
   )
